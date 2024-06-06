96725b7df33dfc621ff52d586d04f81c6284d6ee
Pablo V <maybe_pablo+github@icloud.com>
2024-06-06 18:44:52 +0900

    Modernize mdct.h

--- src/mdct/mdct.h
+++ src/mdct/mdct.h
@@ -44,42 +44,35 @@ class TMDCT : public TMDCTBase {
     std::vector<TFloat> Buf;
 public:
     TMDCT(float scale = 1.0)
-        : TMDCTBase(TN, scale)
-        , Buf(TN/2)
-    {
-    }
-    const std::vector<TFloat>& operator()(TFloat* in) {
+        : TMDCTBase(TN, scale), Buf(TN/2) {}
 
+    const std::vector<TFloat>& operator()(TFloat* in) {
         const size_t n2 = N >> 1;
         const size_t n4 = N >> 2;
         const size_t n34 = 3 * n4;
         const size_t n54 = 5 * n4;
-        const TFloat* cos = &SinCos[0];
-        const TFloat* sin = &SinCos[1];
 
-        TFloat  *xr, *xi, r0, i0;
-        TFloat  c, s;
-        size_t n;
+        const TFloat* cos = SinCos.data();
+        const TFloat* sin = SinCos.data() + 1;
 
-        xr = (TFloat*)FFTIn;
-        xi = (TFloat*)FFTIn + 1;
-        for (n = 0; n < n4; n += 2) {
-            r0 = in[n34 - 1 - n] + in[n34 + n];
-            i0 = in[n4 + n] - in[n4 - 1 - n];
+        TFloat* xr = reinterpret_cast<TFloat*>(FFTIn);
+        TFloat* xi = xr + 1;
 
-            c = cos[n];
-            s = sin[n];
+        for (size_t n = 0; n < n4; n += 2) {
+            const TFloat r0 = in[n34 - 1 - n] + in[n34 + n];
+            const TFloat i0 = in[n4 + n] - in[n4 - 1 - n];
+            const TFloat c = cos[n];
+            const TFloat s = sin[n];
 
             xr[n] = r0 * c + i0 * s;
             xi[n] = i0 * c - r0 * s;
         }
 
-        for (; n < n2; n += 2) {
-            r0 = in[n34 - 1 - n] - in[n - n4];
-            i0 = in[n4 + n]    + in[n54 - 1 - n];
-
-            c = cos[n];
-            s = sin[n];
+        for (size_t n = n4; n < n2; n += 2) {
+            const TFloat r0 = in[n34 - 1 - n] - in[n - n4];
+            const TFloat i0 = in[n4 + n] + in[n54 - 1 - n];
+            const TFloat c = cos[n];
+            const TFloat s = sin[n];
 
             xr[n] = r0 * c + i0 * s;
             xi[n] = i0 * c - r0 * s;
@@ -87,17 +80,17 @@ public:
 
         kiss_fft(FFTPlan, FFTIn, FFTOut);
 
-        xr = (TFloat*)FFTOut;
-        xi = (TFloat*)FFTOut + 1;
-        for (n = 0; n < n2; n += 2) {
-            r0 = xr[n];
-            i0 = xi[n];
+        xr = reinterpret_cast<TFloat*>(FFTOut);
+        xi = xr + 1;
 
-            c = cos[n];
-            s = sin[n];
+        for (size_t n = 0; n < n2; n += 2) {
+            const TFloat r0 = xr[n];
+            const TFloat i0 = xi[n];
+            const TFloat c = cos[n];
+            const TFloat s = sin[n];
 
-            Buf[n] = - r0 * c - i0 * s;
-            Buf[n2 - 1 -n] = - r0 * s + i0 * c;
+            Buf[n] = -r0 * c - i0 * s;
+            Buf[n2 - 1 - n] = -r0 * s + i0 * c;
         }
 
         return Buf;
@@ -109,31 +102,25 @@ class TMIDCT : public TMDCTBase {
     std::vector<TFloat> Buf;
 public:
     TMIDCT(float scale = TN)
-        : TMDCTBase(TN, scale/2)
-        , Buf(TN)
-    {}
-    const std::vector<TFloat>& operator()(TFloat* in) {
+        : TMDCTBase(TN, scale / 2), Buf(TN) {}
 
+    const std::vector<TFloat>& operator()(TFloat* in) {
         const size_t n2 = N >> 1;
         const size_t n4 = N >> 2;
         const size_t n34 = 3 * n4;
         const size_t n54 = 5 * n4;
-        const TFloat* cos = &SinCos[0];
-        const TFloat* sin = &SinCos[1];
 
-        TFloat *xr, *xi, r0, i0, r1, i1;
-        TFloat c, s;
-        size_t n;
+        const TFloat* cos = SinCos.data();
+        const TFloat* sin = SinCos.data() + 1;
 
-        xr = (TFloat*)FFTIn;
-        xi = (TFloat*)FFTIn + 1;
+        TFloat* xr = reinterpret_cast<TFloat*>(FFTIn);
+        TFloat* xi = xr + 1;
 
-        for (n = 0; n < n2; n += 2) {
-            r0 = in[n];
-            i0 = in[n2 - 1 - n];
-
-            c = cos[n];
-            s = sin[n];
+        for (size_t n = 0; n < n2; n += 2) {
+            const TFloat r0 = in[n];
+            const TFloat i0 = in[n2 - 1 - n];
+            const TFloat c = cos[n];
+            const TFloat s = sin[n];
 
             xr[n] = -2.0 * (i0 * s + r0 * c);
             xi[n] = -2.0 * (i0 * c - r0 * s);
@@ -141,18 +128,17 @@ public:
 
         kiss_fft(FFTPlan, FFTIn, FFTOut);
 
-        xr = (TFloat*)FFTOut;
-        xi = (TFloat*)FFTOut + 1;
+        xr = reinterpret_cast<TFloat*>(FFTOut);
+        xi = xr + 1;
 
-        for (n = 0; n < n4; n += 2) {
-            r0 = xr[n];
-            i0 = xi[n];
+        for (size_t n = 0; n < n4; n += 2) {
+            const TFloat r0 = xr[n];
+            const TFloat i0 = xi[n];
+            const TFloat c = cos[n];
+            const TFloat s = sin[n];
 
-            c = cos[n];
-            s = sin[n];
-
-            r1 = r0 * c + i0 * s;
-            i1 = r0 * s - i0 * c;
+            const TFloat r1 = r0 * c + i0 * s;
+            const TFloat i1 = r0 * s - i0 * c;
 
             Buf[n34 - 1 - n] = r1;
             Buf[n34 + n] = r1;
@@ -160,21 +146,21 @@ public:
             Buf[n4 - 1 - n] = -i1;
         }
 
-        for (; n < n2; n += 2) {
-            r0 = xr[n];
-            i0 = xi[n];
+        for (size_t n = n4; n < n2; n += 2) {
+            const TFloat r0 = xr[n];
+            const TFloat i0 = xi[n];
+            const TFloat c = cos[n];
+            const TFloat s = sin[n];
 
-            c = cos[n];
-            s = sin[n];
-
-            r1 = r0 * c + i0 * s;
-            i1 = r0 * s - i0 * c;
+            const TFloat r1 = r0 * c + i0 * s;
+            const TFloat i1 = r0 * s - i0 * c;
 
             Buf[n34 - 1 - n] = r1;
             Buf[n - n4] = -r1;
             Buf[n4 + n] = i1;
             Buf[n54 - 1 - n] = i1;
         }
+
         return Buf;
     }
 };
